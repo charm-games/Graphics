92f92ac74b630ca857f47c7bb79df9027b0859e6
Kleber Garcia <kleber.garcia@unity3d.com>
2021-12-20 17:57:44 -0500

    ** Order independent transparency Branch on GPU pipeline **
    * Formatting

--- com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipelineRuntimeResources.cs
+++ com.unity.render-pipelines.high-definition/Runtime/RenderPipeline/HDRenderPipelineRuntimeResources.cs
@@ -127,6 +127,8 @@ namespace UnityEngine.Rendering.HighDefinition
 
             [Reload("Runtime/Material/Visibility/Visibility.shader")]
             public Shader visibilityPS;
+            [Reload("Runtime/Material/Visibility/VisibilityOIT.shader")]
+            public Shader visibilityOITPS;
             [Reload("Runtime/Material/Visibility/CreateMaterialDepth.shader")]
             public Shader createMaterialDepthPS;
             [Reload("Runtime/Material/Visibility/VBufferClassification.compute")]
